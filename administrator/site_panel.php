fd81153eb60223b8ac364f8086d166dd26a439f6
Radek <radek3301@linux.pl>
2019-09-28 21:53:51 +0200

    update

--- administrator/site_panel.php
+++ administrator/site_panel.php
@@ -78,7 +78,7 @@
                         <div class="section_name">Section 1</div>
                         <div class="section_title">
                             Title<br>
-                            <input type="text" name="title" value="<?php require_once "get_section_01_content.php"; echo $title; ?>">
+                            <input type="text" name="title" value="<?php require_once "get_section_01_content.php"; echo $title_sec_01; ?>">
                         </div>
                         <div class="section_submit">
                             <input type="submit" value="Confirm">
@@ -95,7 +95,7 @@
                         <div class="section_name">Section 2</div>
                         <div class="section_title">
                             Title<br>
-                            <input type="text" name="title" value="<?php require_once "get_section_02_content.php"; echo $title; ?>">
+                            <input type="text" name="title" value="<?php require_once "get_section_02_content.php"; echo $title_sec_02; ?>">
                         </div>
                         <div class="section_submit">
                             <input type="submit" value="Confirm">
@@ -112,7 +112,7 @@
                         <div class="section_name">Section 3</div>
                         <div class="section_title">
                             Title<br>
-                            <input type="text" name="title" value="<?php require_once "get_section_03_content.php"; echo $title; ?>">
+                            <input type="text" name="title" value="<?php require_once "get_section_03_content.php"; echo $title_sec_03; ?>">
                         </div>
                         <div class="section_submit">
                             <input type="submit" value="Confirm">
