bb61a5bf4876e10aca41bbe21dfdc1db0bd6894f
Radek <radek3301@linux.pl>
2019-10-03 19:43:09 +0200

    update add user sections - added notification

--- administrator/post_add_new_user.php
+++ administrator/post_add_new_user.php
@@ -7,6 +7,30 @@
         exit();
     }
 
+    $username = $_POST['username'];
+    $confirm_username = $_POST['confirm_username'];
+
+    $password = $_POST['password'];
+    $confirm_password = $_POST['confirm_password'];
+
+    $email = $_POST['e-mail'];
+    $confirm_email = $_POST['confirm_e-mail'];
+
+    if ( $username != $confirm_username || $password != $confirm_password || $email != $confirm_email )
+    {
+        header('Location: add_user_panel.php');
+        $_SESSION['add_user_error'] = "Incorrect login details";
+        exit();
+    }
+    elseif (empty($username) || empty($password) || empty($email) )
+    {
+        header('Location: add_user_panel.php');
+        $_SESSION['add_user_error'] = "Login details are incomplete";
+        exit();
+    }
+    echo "men";
+
+
     require_once "connect.php";
 
     $connection = @new mysqli($host, $db_user, $db_password, $db_name);
@@ -16,40 +40,51 @@
     }
     else
     {
-        $username = $_POST['username'];
-        $confirm_username = $_POST['confirm_username'];
+        $is_OK = true;
 
-        $password = $_POST['password'];
-        $confirm_password = $_POST['confirm_password'];
+        //Check username
 
-        $email = $_POST['e-mail'];
-        $confirm_email = $_POST['confirm_e-mail'];
+        $result = $connection->query("SELECT id FROM administrators WHERE username = '$username'");
 
-        if ( $username != $confirm_username || $password != $confirm_password || $email != $confirm_email )
+        if (!$result) throw new Exception($connection->error);
+
+        $count_usernames = $result->num_rows;
+        if($count_usernames > 0)
         {
-            header('Location: add_user_panel.php');
-            $_SESSION['add_user_error'] = "Incorrect login details";
-            exit();
+            $is_OK = false;
+            $_SESSION['add_user_error'] = "User already exists";
         }
-        elseif (empty($username) || empty($password) || empty($email) )
+
+        //Check password
+
+        $result = $connection->query("SELECT id FROM administrators WHERE mail = '$email'");
+
+        if (!$result) throw new Exception($connection->error);
+
+        $count_emails = $result->num_rows;
+        if($count_emails > 0)
         {
-            header('Location: add_user_panel.php');
-            $_SESSION['add_user_error'] = "Login details are incomplete";
-            exit();
+            $is_OK = false;
+            $_SESSION['add_user_error'] = "Email already exists";
         }
 
-        $sql = "INSERT INTO administrators (username, pass, mail) VALUES ('$username', '$password', '$email');";
-
-        if ( $connection->query($sql) != true )
+        if ($is_OK == true)
         {
-            echo "Error: " . $connection->connect_errno;
+            if ($connection->query("INSERT INTO administrators VALUES (NULL, '$username', '$password', '$email');"))
+            {
+                $_SESSION['user_passed'] = true;
+
+                if (isset($_SESSION['add_user_error'])) {
+                    unset($_SESSION['add_user_error']);
+                }
+            }
+            else
+            {
+                $_SESSION['add_user_error'] = "User already exists";
+            }
         }
 
-        if (isset($_SESSION['add_user_error'])) {
-            unset($_SESSION['add_user_error']);
-        }
-
-        $connection->close();
         header('Location: add_user_panel.php');
+        $connection->close();
     }
 ?>
