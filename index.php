b6e6f64748bd82e78f99ab861001766dd4f1f542
Radek <radek3301@linux.pl>
2019-10-15 00:19:02 +0200

    made new front layout

--- index.php
+++ index.php
@@ -16,47 +16,12 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="styles/scrollbar.css">
-    <link rel="stylesheet" href="styles/max_width_1300.css">
-    <link rel="stylesheet" href="styles/max_width_1000.css">
     <link rel="stylesheet" href="administrator/admin_styles/top_bar.css">
     <link rel="stylesheet" href="styles/links.css">
     <link rel="stylesheet" href="styles/nav_bar.css">
+    <link rel="stylesheet" href="styles/separator.css">
 
     <script src="scripts/jquery-3.4.1.min.js"></script>
-
-    <?php
-        if ( isset($_SESSION['logged']) )
-        {
-            $display = "block";
-            $nav_margin = "40px";
-            $content_margin = "40px";
-        }
-        else
-        {
-            $display = "none";
-            $nav_margin = "0px";
-            $content_margin = "0px";
-        }
-    ?>
-
-    <style>
-        #top_bar
-        {
-            display: <?php echo $display; ?>;
-        }
-
-        #nav_vertical_panel, #nav_horizontal_panel
-        {
-            top: <?php echo $nav_margin; ?>;
-        }
-
-        #container
-        {
-            margin-top: calc(<?php echo $content_margin; ?> + 70px);
-            padding: 40px;
-            text-align: justify;
-        }
-    </style>
 
     <title>
         <?php
@@ -66,41 +31,131 @@
     </title>
 </head>
 <body>
-    <div id="screen_dimming"></div>
 
-    <div id="nav_horizontal_panel">
-        <div id="logo">
-            <a href="index.php">
+    <div id="container">
+
+        <div id="screen_dimming"></div>
+        <div class="container_section bg_00">
+            <div id="nav_horizontal_panel">
+                <img id="show_vertical_panel" class="slide_menu_button" src="images/slide_menu_light.svg">
+                <div class="nav_button left" id="button_sec_01">
+                    <a href="javascript:void();" onclick="scroll_to('#sec_01');">
+                        <?php
+                            require_once "administrator/get_section_01_content.php";
+                            echo $title_sec_01;
+                        ?>
+                    </a>
+                </div>
+                <div class="nav_button left" id="button_sec_02">
+                    <a href="javascript:void();" onclick="scroll_to('#sec_02');">
+                        <?php
+                            require_once "administrator/get_section_02_content.php";
+                            echo $title_sec_02;
+                        ?>
+                    </a>
+                </div>
+                <div class="nav_button left" id="button_sec_03">
+                    <a href="javascript:void();" onclick="scroll_to('#sec_03');">
+                        <?php
+                            require_once "administrator/get_section_03_content.php";
+                            echo $title_sec_03;
+                        ?>
+                    </a>
+                </div>
+                <div id="nav_logo" class="right">
+                    <?php
+                        require_once "administrator/get_page_title.php";
+                        echo $page_title;
+                    ?>
+                </div>
+            </div>
+            <div class="content">
+                <div class="welcome_text text_center">
+                    <?php
+                        require_once "administrator/get_welcome_content.php";
+                        echo $welcome_content;
+                    ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="separator">
+            <div class="separator_title" id="sec_01">
                 <?php
-                    require_once "administrator/get_page_title.php";
-                    echo $page_title;
+                    require_once "administrator/get_section_01_content.php";
+                    echo $title_sec_01;
                 ?>
-            </a>
+            </div>
         </div>
-        <div id="nav_show_menu_button">
-            <img src="images/nav-menu-button.svg">
+
+        <div class="container_section bg_01">
+            <div class="content">
+                <div class="content_text text_just">
+                    <?php
+                        require_once "administrator/get_section_01_content.php";
+                        echo $content_sec_01;
+                    ?>
+                </div>
+            </div>
         </div>
+
+        <div class="separator" id="sec_02">
+            <div class="separator_title">
+                <?php
+                    require_once "administrator/get_section_02_content.php";
+                    echo $title_sec_02;
+                ?>
+            </div>
+        </div>
+
+        <div class="container_section bg_02">
+            <div class="content">
+                <div class="content_text text_just">
+                    <?php
+                        require_once "administrator/get_section_02_content.php";
+                        echo $content_sec_02;
+                    ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="separator" id="sec_03">
+            <div class="separator_title">
+                <?php
+                    require_once "administrator/get_section_03_content.php";
+                    echo $title_sec_03;
+                ?>
+            </div>
+        </div>
+
+        <div class="container_section bg_03">
+            <div class="content">
+                <div class="content_text text_just">
+                    <?php
+                        require_once "administrator/get_section_03_content.php";
+                        echo $content_sec_03;
+                    ?>
+                </div>
+            </div>
+        </div>
+
     </div>
 
     <div id="nav_vertical_panel">
-        <div id="nav_hide_menu_button">
-            <div id="nav_list_menu_button">
-                <img src="images/nav-menu-button.svg">
-            </div>
-        </div>
-        <div class="nav_bar_button" id="button_sec_01">
+        <img id="hide_vertical_panel" class="slide_menu_button" src="images/slide_menu_light.svg">
+        <div class="nav_vertical_button" id="button_sec_01">
             <?php
                 require_once "administrator/get_section_01_content.php";
                 echo $title_sec_01;
             ?>
         </div>
-        <div class="nav_bar_button" id="button_sec_02">
+        <div class="nav_vertical_button" id="button_sec_02">
             <?php
                 require_once "administrator/get_section_02_content.php";
                 echo $title_sec_02;
             ?>
         </div>
-        <div class="nav_bar_button" id="button_sec_03">
+        <div class="nav_vertical_button" id="button_sec_03">
             <?php
                 require_once "administrator/get_section_03_content.php";
                 echo $title_sec_03;
@@ -108,49 +163,6 @@
         </div>
     </div>
 
-    <div id="container">
-        <div class="content" id="welcone_content">
-            <?php
-                require_once "administrator/get_welcome_content.php";
-                echo $welcome_content;
-            ?>
-        </div>
-        <div class="content" id="section_01">
-            <?php
-                require_once "administrator/get_section_01_content.php";
-                echo $content_sec_01;
-            ?>
-        </div>
-        <div class="content" id="section_02">
-            <?php
-                require_once "administrator/get_section_02_content.php";
-                echo $content_sec_02;
-            ?>
-        </div>
-        <div class="content" id="section_03">
-            <?php
-                require_once "administrator/get_section_03_content.php";
-                echo $content_sec_03;
-            ?>
-        </div>
-    </div>
-
-    <div id="top_bar">
-        <div class="top_bar_button left">
-            <a href="index.php">Test site</a>
-        </div>
-        <div class="top_bar_button left">
-            <a href="administrator/index.php">Admin panel</a>
-        </div>
-        <div class="top_bar_button right">
-            <a href="administrator/logout.php">Log out</a>
-        </div>
-        <div class="top_bar_button right">
-            <a href="administrator/user_panel.php">
-                <?php echo $_SESSION['user'] . ""; ?>
-            </a>
-        </div>
-    </div>
 
     <script src="scripts/main.js"></script>
 </body>
--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,22 @@
+window.onload = delay_show_element();
+
+function delay_show_element(element)
+{
+    let delayInMilliseconds = 500;
+    setTimeout(function() {
+        $(".welcome_text").fadeIn();
+    }, delayInMilliseconds);
+}
+
+function scroll_to(selector) {
+    $('html,body').animate({scrollTop: $(selector).offset().top}, 1000);
+    return false;
+}
+
 $(document).ready( () =>
 {
-
     $("#button_sec_01").click( () =>
     {
-        $("#welcone_content").hide();
-
-        $("#section_01").css("display", "block");
-        $("#section_01").animate({opacity: 1});
-
-        $("#section_02").css("display", "none");
-        $("#section_02").animate({opacity: 0});
-
-        $("#section_03").css("display", "none");
-        $("#section_03").animate({opacity: 0});
-
         if($('#nav_vertical_panel').css('display') == "block")
         {
             $("#nav_vertical_panel").slideUp();
@@ -23,17 +26,6 @@ $(document).ready( () =>
 
     $("#button_sec_02").click( () =>
     {
-        $("#welcone_content").hide();
-
-        $("#section_01").css("display", "none");
-        $("#section_01").animate({opacity: 0});
-
-        $("#section_02").css("display", "block");
-        $("#section_02").animate({opacity: 1});
-
-        $("#section_03").css("display", "none");
-        $("#section_03").animate({opacity: 0});
-
         if($('#nav_vertical_panel').css('display') == "block")
         {
             $("#nav_vertical_panel").slideUp();
@@ -43,17 +35,6 @@ $(document).ready( () =>
 
     $("#button_sec_03").click( () =>
     {
-        $("#welcone_content").hide();
-
-        $("#section_01").css("display", "none");
-        $("#section_01").animate({opacity: 0});
-
-        $("#section_02").css("display", "none");
-        $("#section_02").animate({opacity: 0});
-
-        $("#section_03").css("display", "block");
-        $("#section_03").animate({opacity: 1});
-
         if($('#nav_vertical_panel').css('display') == "block")
         {
             $("#nav_vertical_panel").slideUp();
@@ -61,13 +42,13 @@ $(document).ready( () =>
         }
     });
 
-    $("#nav_show_menu_button").click( () =>
+    $("#show_vertical_panel").click( () =>
     {
         $("#nav_vertical_panel").slideDown();
         $("#screen_dimming").fadeIn();
     });
 
-    $("#nav_hide_menu_button").click( () =>
+    $("#hide_vertical_panel").click( () =>
     {
         $("#nav_vertical_panel").slideUp();
         $("#screen_dimming").fadeOut();
