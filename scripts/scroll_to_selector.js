62e2bed24bbc18f4465ec6248bc8623d6d394577
Radek <radek3301@linux.pl>
2019-10-24 23:09:55 +0200

    update analog clock

--- scripts/scroll_to_selector.js
+++ scripts/scroll_to_selector.js
@@ -1,18 +1,16 @@
-const to_admin_panel = document.querySelector(".to_admin_panel");
-const to_logout = document.querySelector(".to_logout");
-const scroll_to_sec_1 = document.querySelector(".to_sec_1");
-const scroll_to_sec_2 = document.querySelector(".to_sec_2");
-const scroll_to_sec_3 = document.querySelector(".to_sec_3");
-const page_reload = document.querySelector(".page_reload");
+const to_admin_panel = document.querySelectorAll(".to_admin_panel");
+const to_logout = document.querySelectorAll(".to_logout");
+const scroll_to_sec_1 = document.querySelectorAll(".to_sec_1");
+const scroll_to_sec_2 = document.querySelectorAll(".to_sec_2");
+const scroll_to_sec_3 = document.querySelectorAll(".to_sec_3");
+const page_reload = document.querySelectorAll(".page_reload");
 
-if (to_admin_panel) { to_admin_panel.addEventListener("click", () => location.href = 'administrator/site_panel.php'); }
-if (to_logout) { to_logout.addEventListener("click", () => location.href = 'administrator/logout.php'); }
-
-scroll_to_sec_1.addEventListener("click", () => scroll_to('#sec_01'));
-scroll_to_sec_2.addEventListener("click", () => scroll_to('#sec_02'));
-scroll_to_sec_3.addEventListener("click", () => scroll_to('#sec_03'));
-page_reload.addEventListener("click", () => location.reload());
-
+to_admin_panel.forEach( (tag_element) => { tag_element.addEventListener("click", () => location.href = 'administrator/site_panel.php'); });
+to_logout.forEach( (tag_element) => { tag_element.addEventListener("click", () => location.href = 'administrator/logout.php'); });
+scroll_to_sec_1.forEach( (tag_element) => { tag_element.addEventListener("click", () => scroll_to('#sec_01')); });
+scroll_to_sec_3.forEach( (tag_element) => { tag_element.addEventListener("click", () => scroll_to('#sec_02')); });
+scroll_to_sec_3.forEach( (tag_element) => { tag_element.addEventListener("click", () => scroll_to('#sec_03')); });
+page_reload.forEach( (tag_element) => { tag_element.addEventListener("click", () => location.reload()) });
 
 const scroll_to = (selector) =>
 {
