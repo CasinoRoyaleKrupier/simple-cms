abfcfe7840abe25b3ed6d04bd0187d8c5a49e9e6
radlesner <radek3301@linux.pl>
2021-10-08 23:21:05 +0200

    Update connect.php file.

--- administrator/connect.php
+++ administrator/connect.php
@@ -1,8 +1,13 @@
 <?php
 
     $host = "localhost";
-    $db_user = "root";
-    $db_password = "";
+    $db_user = "simplecms";
+    $db_password = "Zaq1Xsw2";
     $db_name = "simple_cms"
+
+    // $host = "localhost";
+    // $db_user = "root";
+    // $db_password = "";
+    // $db_name = "simple_cms"
 
 ?>
